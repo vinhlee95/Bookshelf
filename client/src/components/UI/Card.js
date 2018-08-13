6b7f3f369d29dc36430270a899613e3758262570
Vinhlee95 <lethanhvinh95@gmail.com>
2018-08-13 09:13:59 +0300

    add delete button

--- client/src/components/UI/Card.js
+++ client/src/components/UI/Card.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
+import {Card, Button} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const styles = {
+  bottomContainer: {
+    display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end'
+  }
+}
 
 const SimpleCard = (props) => {
   return (
       <Card className='card'>
           <h2> {props.bookName}</h2>
-          <div>
+          <div style={styles.bottomContainer}>
+            <div>
             <p>{props.genre}</p>
-            <p>{props.authorName}</p>
+            <p style={{ marginBottom: 0 }}>{props.authorName}</p>
+            </div>
+            <Button 
+              variant="fab" 
+              color='secondary' 
+              aria-label="Delete"
+              style={{ height: 40, width: 40 }} >
+              <DeleteIcon />
+            </Button>
           </div>
       </Card>
   );
